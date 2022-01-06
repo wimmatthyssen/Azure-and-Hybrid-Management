08539a42b2f45edeeb95ff4fa8497a65d44db97d
Wim Matthyssen <62010638+wimmatthyssen@users.noreply.github.com>
2022-01-06 20:37:50 +0100

    Update Generate-authenticaton-certificates-P2S-VPN.ps1

--- Generate-authenticaton-certificates-P2S-VPN.ps1
+++ Generate-authenticaton-certificates-P2S-VPN.ps1
@@ -30,8 +30,8 @@ Disclaimer:     This script is provided "As Is" with no warranties.
 .\Generate-authenticaton-certificates-P2S-VPN.ps1
 
 .LINK
-https://wmatthyssen.com/2021/12/15/powershell-script-generate-a-self-signed-root-certificate-and-client-certificate-for-use-with-an-azure-p2s-vpn/
 
+https://wmatthyssen.com/2021/12/15/powershell-script-generate-a-self-signed-root-certificate-and-client-certificate-for-use-with-an-azure-p2s-vpn/
 #>
 
 ## ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
