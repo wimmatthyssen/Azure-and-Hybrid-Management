0d136c1555bbe4ed3b693baf0f200b2900d66314
Wim Matthyssen <62010638+wimmatthyssen@users.noreply.github.com>
2022-04-21 16:47:32 +0200

    Update Build-ManagementGroups-Tree-Hierarchy.ps1

--- Build-ManagementGroups-Tree-Hierarchy.ps1
+++ Build-ManagementGroups-Tree-Hierarchy.ps1
@@ -1,33 +1,35 @@
 <#
 .SYNOPSIS
 
-A script used to build a management groups tree structure.
+A script used to create a management groups tree structure
 
 .DESCRIPTION
 
-A script used to build a management groups tree structure based on the Enterprise-scale architecture with hub and spoke architecture.
+A script used to create a management groups tree structure. 
+When all management groups are created the Azure subscriptions will be moved to the corresponding management group.
 
 .NOTES
 
-Filename:       Build-ManagementGroups-Tree-Hierarchy.ps1
+Filename:       Create-Azure-Management-Groups-Tree-Hierarchy.ps1
 Created:        31/07/2020
-Last modified:  29/06/2021
+Last modified:  21/04/2022
 Author:         Wim Matthyssen
-PowerShell:     Azure Cloud Shell or Azure PowerShell
-Version:        Install latest Azure Powershell modules
+PowerShell:     Azure PowerShell or Azure Cloud Shell
+Version:        Install latest Azure PowerShell modules
 Action:         Change variables were needed to fit your needs. 
 Disclaimer:     This script is provided "As Is" with no warranties.
 
+
 .EXAMPLE
 
-Build-ManagementGroups-Tree-Hierarchy.ps1
+.\Create-Azure-Management-Groups-Tree-Hierarchy.ps1
 
 .LINK
 
 https://wmatthyssen.com/2020/08/01/azure-powershell-script-create-a-management-group-tree-hierarchy/
 #>
 
-## ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+## ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 ## Variables
 
@@ -60,100 +62,198 @@ $onlineManagementGroupGuid = New-Guid
 $sapManagementGroupName = "mg-" + $companyShortName + "-sap"
 $sapManagementGroupGuid = New-Guid
 
+$subNameManagement = Get-AzSubscription | Where-Object {$_.Name -like "*management*"}
+$subNameConnectivity = Get-AzSubscription | Where-Object {$_.Name -like "*connectivity*"}
+$subNameIdentity = Get-AzSubscription | Where-Object {$_.Name -like "*identity*"}
+$subNameCorpPrd = Get-AzSubscription | Where-Object {$_.Name -like "*prd*corp*"}
+$subNameCorpDev = Get-AzSubscription | Where-Object {$_.Name -like "*dev*corp*"}
+$subNameOnlinePrd = Get-AzSubscription | Where-Object {$_.Name -like "*prd*online*"}
+$subNameOnlineDev = Get-AzSubscription | Where-Object {$_.Name -like "*dev*online*"}
+$subNameSapPrd = Get-AzSubscription | Where-Object {$_.Name -like "*prd*sap*"}
+$subNameSapDev = Get-AzSubscription | Where-Object {$_.Name -like "*dev*sap*"}
+$subNameTest = Get-AzSubscription | Where-Object {$_.Name -like "*tst*"}
+
 $global:currenttime= Set-PSBreakpoint -Variable currenttime -Mode Read -Action {$global:currenttime= Get-Date -UFormat "%A %m/%d/%Y %R"}
 $foregroundColor1 = "Red"
 $foregroundColor2 = "Yellow"
 $writeEmptyLine = "`n"
 $writeSeperatorSpaces = " - "
 
-## ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+## ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-## Prerequisites
-
-## Check if running as Administrator (when not running from Cloud Shell), otherwise close the PowerShell window
+## Check if PowerShell runs as Administrator (when not running from Cloud Shell), otherwise exit the script
 
 if ($PSVersionTable.Platform -eq "Unix") {
-    Write-Host ($writeEmptyLine + "# Running in Cloud Shell" + $writeSeperatorSpaces + $currentTime)
+    Write-Host ($writeEmptyLine + "# Running in Cloud Shell" + $writeSeperatorSpaces + $currentTime)`
+    -foregroundcolor $foregroundColor1 $writeEmptyLine
+    
+    ## Start script execution    
+    Write-Host ($writeEmptyLine + "# Script started. Without any errors, it will need around 5 minute to complete" + $writeSeperatorSpaces + $currentTime)`
+    -foregroundcolor $foregroundColor1 $writeEmptyLine 
 } else {
     $currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
     $isAdministrator = $currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
 
-    if ($isAdministrator -eq $false) {
-    Write-Host ($writeEmptyLine + "# Please run PowerShell as Administrator" + $writeSeperatorSpaces + $currentTime +$writeEmptyLine)`
-    -foregroundcolor $foregroundColor1
-        Start-Sleep -s 4
-    exit} else {
-        ## Import Az module into the PowerShell session
-        Import-Module Az
-        Write-Host ($writeEmptyLine + "# Az module imported" + $writeSeperatorSpaces + $currentTime)`
+        ## Check if running as Administrator, otherwise exit the script
+        if ($isAdministrator -eq $false) {
+        Write-Host ($writeEmptyLine + "# Please run PowerShell as Administrator" + $writeSeperatorSpaces + $currentTime)`
         -foregroundcolor $foregroundColor1 $writeEmptyLine
-    }
+        Start-Sleep -s 3
+        exit
+        }
+        else {
+
+        ## If running as Administrator, start script execution    
+        Write-Host ($writeEmptyLine + "# Script started. Without any errors, it will need around 5 minutes to complete" + $writeSeperatorSpaces + $currentTime)`
+        -foregroundcolor $foregroundColor1 $writeEmptyLine 
+        }
 }
+
+## ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 ## Suppress breaking change warning messages
 
 Set-Item Env:\SuppressAzurePowerShellBreakingChangeWarnings "true"
 
-## ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+## ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-## Deployment started
+## Create Company management group
 
-Write-Host ($writeEmptyLine + "# Deployment started" + $writeSeperatorSpaces + $currentTime)`
--foregroundcolor $foregroundColor1 $writeEmptyLine
+New-AzManagementGroup -GroupName $companyManagementGroupGuid -DisplayName $companyManagementGroupName | Out-Null
 
-## ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# Store Company management group in a variable
+$companyParentGroup = Get-AzManagementGroup -GroupName $companyManagementGroupGuid
 
-## Create company (or company) management group
-
-New-AzManagementGroup -GroupId $companyManagementGroupGuid -DisplayName $companyManagementGroupName
-
-$companyParentGroup = Get-AzManagementGroup -GroupId $companyManagementGroupGuid
-
-Write-Host ($writeEmptyLine + "# Company management group created" + $writeSeperatorSpaces + $currentTime)`
+Write-Host ($writeEmptyLine + "# Company management group $companyManagementGroupName created" + $writeSeperatorSpaces + $currentTime)`
 -foregroundcolor $foregroundColor2 $writeEmptyLine
 
-## ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+## ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-## Create top management groups (Platform, Landing Zones, Sandboxes, Decommissioned)
+## Create Top management groups
 
-New-AzManagementGroup -GroupId $platformManagementGroupGuid -DisplayName $platformManagementGroupName -ParentObject $companyParentGroup
-New-AzManagementGroup -GroupId $landingZonesManagementGroupGuid -DisplayName $landingZonesManagementGroupName -ParentObject $companyParentGroup
-New-AzManagementGroup -GroupId $sandboxesManagementGroupGuid -DisplayName $sandboxesManagementGroupName -ParentObject $companyParentGroup
-New-AzManagementGroup -GroupId $decommissionedManagementGroupGuid -DisplayName $decommissionedManagementGroupName -ParentObject $companyParentGroup
+# Create Platform management group
+New-AzManagementGroup -GroupName $platformManagementGroupGuid -DisplayName $platformManagementGroupName -ParentObject $companyParentGroup | Out-Null
 
-$platformParentGroup = Get-AzManagementGroup -GroupId $platformManagementGroupGuid 
-$landingZonesParentGroup = Get-AzManagementGroup -GroupId $landingZonesManagementGroupGuid
+# Create Landing Zones management group
+New-AzManagementGroup -GroupName $landingZonesManagementGroupGuid -DisplayName $landingZonesManagementGroupName -ParentObject $companyParentGroup | Out-Null
 
-Write-Host ($writeEmptyLine + "# Top management groups created" + $writeSeperatorSpaces + $currentTime)`
--foregroundcolor $foregroundColor2 $writeEmptyLine
+# Create Sandbox management group
+New-AzManagementGroup -GroupName $sandboxesManagementGroupGuid -DisplayName $sandboxesManagementGroupName -ParentObject $companyParentGroup | Out-Null
 
-## ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# Create Decomission management group
+New-AzManagementGroup -GroupName $decommissionedManagementGroupGuid -DisplayName $decommissionedManagementGroupName -ParentObject $companyParentGroup | Out-Null
+
+# Store specific Top management groups in variables
+$platformParentGroup = Get-AzManagementGroup -GroupName $platformManagementGroupGuid 
+$landingZonesParentGroup = Get-AzManagementGroup -GroupName $landingZonesManagementGroupGuid
+
+Write-Host ($writeEmptyLine + "# Top management groups $platformManagementGroupName, $landingZonesManagementGroupName, $sandboxesManagementGroupName, and `
+$decommissionedManagementGroupName created" + $writeSeperatorSpaces + $currentTime) -foregroundcolor $foregroundColor2 $writeEmptyLine
+
+## ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 ## Create Platform management groups
 
-New-AzManagementGroup -GroupName $managementManagementGroupGuid -DisplayName $managementManagementGroupName -ParentObject $platformParentGroup
-New-AzManagementGroup -GroupName $connectivityManagementGroupGuid -DisplayName $connectivityManagementGroupName -ParentObject $platformParentGroup
-New-AzManagementGroup -GroupName $identityManagementGroupGuid -DisplayName $identityManagementGroupName -ParentObject $platformParentGroup
+# Create Management management group
+New-AzManagementGroup -GroupName $managementManagementGroupGuid -DisplayName $managementManagementGroupName -ParentObject $platformParentGroup | Out-Null
 
-Write-Host ($writeEmptyLine + "# Platform management groups created" + $writeSeperatorSpaces + $currentTime)`
--foregroundcolor $foregroundColor2 $writeEmptyLine
+# Create Connectivity management group
+New-AzManagementGroup -GroupName $connectivityManagementGroupGuid -DisplayName $connectivityManagementGroupName -ParentObject $platformParentGroup | Out-Null
 
-## ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# Create Identity management group
+New-AzManagementGroup -GroupName $identityManagementGroupGuid -DisplayName $identityManagementGroupName -ParentObject $platformParentGroup | Out-Null
+
+Write-Host ($writeEmptyLine + "# Platform management groups $managementManagementGroupName, $connectivityManagementGroupName and `
+$identityManagementGroupName created" + $writeSeperatorSpaces + $currentTime) -foregroundcolor $foregroundColor2 $writeEmptyLine
+
+## ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 ## Create Landing Zones management groups
 
-New-AzManagementGroup -GroupName $corpManagementGroupGuid -DisplayName $corpManagementGroupName -ParentObject $landingZonesParentGroup
-New-AzManagementGroup -GroupName $onlineManagementGroupGuid -DisplayName $onlineManagementGroupName -ParentObject $landingZonesParentGroup
-New-AzManagementGroup -GroupName $sapManagementGroupGuid -DisplayName $sapManagementGroupName -ParentObject $landingZonesParentGroup
+# Create Corp management group
+New-AzManagementGroup -GroupName $corpManagementGroupGuid -DisplayName $corpManagementGroupName -ParentObject $landingZonesParentGroup | Out-Null
 
-Write-Host ($writeEmptyLine + "# Landing Zones management groups created" + $writeSeperatorSpaces + $currentTime)`
+# Create Online management group
+New-AzManagementGroup -GroupName $onlineManagementGroupGuid -DisplayName $onlineManagementGroupName -ParentObject $landingZonesParentGroup | Out-Null
+
+# Create SAP management group
+New-AzManagementGroup -GroupName $sapManagementGroupGuid -DisplayName $sapManagementGroupName -ParentObject $landingZonesParentGroup | Out-Null
+
+Write-Host ($writeEmptyLine + "# Landing Zones management groups $corpManagementGroupName, $onlineManagementGroupName and `
+$sapManagementGroupName created" + $writeSeperatorSpaces + $currentTime) -foregroundcolor $foregroundColor2 $writeEmptyLine
+
+## ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+## Move subscriptions under the tenant root group to the correct management groups, if they exist
+
+# Move Management subscription, if it exists
+If(!! $subNameManagement)
+{
+    New-AzManagementGroupSubscription -GroupId $managementManagementGroupGuid -SubscriptionId $subNameManagement.SubscriptionId
+}
+
+# Move Connectivity subscription, if it exists
+If(!! $subNameConnectivity)
+{
+    New-AzManagementGroupSubscription -GroupId $connectivityManagementGroupGuid -SubscriptionId $subNameConnectivity.SubscriptionId
+}
+
+# Move Identity subscription, if it exists
+If(!! $subNameIdentity)
+{
+    New-AzManagementGroupSubscription -GroupId $identityManagementGroupGuid -SubscriptionId $subNameIdentity.SubscriptionId
+}
+
+# Move Corp Production subscription, if it exists
+If(!! $subNameCorpPrd)
+{
+    New-AzManagementGroupSubscription -GroupId $corpManagementGroupGuid  -SubscriptionId $subNameCorpPrd.SubscriptionId
+}
+
+# Move Corp Development subscription, if it exists
+If(!! $subNameCorpDev)
+{
+    New-AzManagementGroupSubscription -GroupId $corpManagementGroupGuid  -SubscriptionId $subNameCorpDev.SubscriptionId
+}
+
+# Move Online Production subscription, if it exists
+If(!! $subNameOnlinePrd)
+{
+    New-AzManagementGroupSubscription -GroupId $onlineManagementGroupGuid -SubscriptionId $subNameOnlinePrd.SubscriptionId
+}
+
+# Move Online Development subscription, if it exists
+If(!! $subNameOnlineDev )
+{
+    New-AzManagementGroupSubscription -GroupId $onlineManagementGroupGuid -SubscriptionId $subNameOnlineDev.SubscriptionId
+}
+
+# Move SAP Production subscription, if it exists
+If(!! $subNameSapPrd)
+{
+    New-AzManagementGroupSubscription -GroupID $sapManagementGroupGuid -SubscriptionId $subNameSapPrd.SubscriptionId
+}
+
+# Move SAP Development subscription, if it exists
+If(!! $subNameSapDev)
+{
+    New-AzManagementGroupSubscription -GroupID $sapManagementGroupGuid -SubscriptionId $subNameSapDev.SubscriptionId
+}
+
+# Move Test subscription, if it exists
+If(!! $subNameTest)
+{
+    New-AzManagementGroupSubscription -GroupId $sandboxesManagementGroupGuid  -SubscriptionId $subNameTest.SubscriptionId
+}
+
+Write-Host ($writeEmptyLine + "# Subscriptions moved to management groups" + $writeSeperatorSpaces + $currentTime)`
 -foregroundcolor $foregroundColor2 $writeEmptyLine
 
-## ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+## ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-## Deployment completed
+## Write script completed
 
-Write-Host ($writeEmptyLine + "# Deployment completed" + $writeSeperatorSpaces + $currentTime)`
--foregroundcolor $foregroundColor1 $writeEmptyLine
+Write-Host ($writeEmptyLine + "# Script completed" + $writeSeperatorSpaces + $currentTime)`
+-foregroundcolor $foregroundColor1 $writeEmptyLine 
 
-## --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+## ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
