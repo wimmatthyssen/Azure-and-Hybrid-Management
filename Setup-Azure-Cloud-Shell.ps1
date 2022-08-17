04992aa25ad5859ad4095a10bbe332b572c36b81
Wim Matthyssen <62010638+wimmatthyssen@users.noreply.github.com>
2022-08-17 21:34:37 +0200

    Update Setup-Azure-Cloud-Shell.ps1

--- Setup-Azure-Cloud-Shell.ps1
+++ Setup-Azure-Cloud-Shell.ps1
@@ -2,25 +2,29 @@
 
 .SYNOPSIS
 
-A script used to setup Azure Cloud Shell for an Azure environment.
+A script used to setup Cloud Shell for an Azure environment.
 
 .DESCRIPTION
 
-A script used to setup Azure Cloud Shell for an Azure environment.
-The script will first change the current context to use the management subscription.
-Then it will store a set of specified tags into a hash table.
-Next it will create a resource group for Cloud Shell resources, if it not already exists.
-Then it will create a general purpose v2 storage account with specific configuration settings (like minimum TLS version set to 1.2, allow public access disabled), if it not already exists.
-And at the end it will create an Azure File Share with a size of 6 GiB, if it not already exists.
+A script used to setup Cloud Shell for an Azure environment.
+The script will do all of the following:
+
+Check if the PowerShell window is running as Administrator (when not running from Cloud Shell), otherwise the Azure PowerShell script will be exited.
+Suppress breaking change warning messages.
+Change the current context to use a management subscription (a subscription with *management* in the subscription name will be automatically selected).
+Store a specified set of tags in a hash table.
+Create a resource group for the Cloud Shell resources if it not already exists. Also apply the necessary tags to this resource group.
+Create a general purpose v2 storage account for Cloud Shell with specific configuration settings (like minimum TLS version set to 1.2, allow public access set to disabled), if it not already exists. Also apply the necessary tags to this storage account.
+Create an Azure file share for Cloud Shell with a size of 6 GiB if it not already exists. Also apply the necessary meta data to this file share.
 
 .NOTES
 
 Filename:       Setup-Azure-Cloud-Shell.ps1
 Created:        30/07/2020
-Last modified:  29/07/2022
+Last modified:  17/08/2022
 Author:         Wim Matthyssen
 Version:        2.0
-PowerShell:     Azure PowerShell
+PowerShell:     PowerShell 5.1 and Azure PowerShell
 Requires:       PowerShell Az (v5.9.0) Module
 Action:         Change variables where needed to fit your needs
 Disclaimer:     This script is provided "As Is" with no warranties
@@ -41,14 +45,13 @@ https://wmatthyssen.com/2022/02/23/setup-azure-cloud-shell-with-azure-powershell
 
 ## Variables
 
-$companyShortName = #"<your company short name here>" The three-letter abbreviation of your company name here. Example: "myh"
 $spoke = "hub"
 $region = #<your region here> The used Azure public region. Example: "westeurope"
 $purpose = "CloudShell"
 
-$rgStorageName = #<your Azure Cloud Shell rg here> The new Azure resource group in which the new Cloud Shell resources will be created. Example: "rg-hub-myh-storage-01""
-$cloudShellStorageAccountName = #<your storage account name here> The name of the storage account used by Cloud Shell. Example: "stlrshubmyhcs"
-$storageSkuNameStandardLrs = "Standard_LRS"
+$rgNameStorage = #<your Azure Cloud Shell rg here> The new Azure resource group in which the new Cloud Shell resources will be created. Example: "rg-hub-myh-storage-01""
+$cloudShellStorageAccountName = #<your storage account name here> The name of the storage account used by Cloud Shell. Example: "sthubmyhcs"
+$storageAccountSkuName = "Standard_LRS"
 $storageAccountType = "StorageV2"
 $storageMinimumTlsVersion = "TLS1_2"
 
@@ -63,6 +66,8 @@ $tagCriticalityName = #<your businessCriticality tag name here> The businessCrit
 $tagCriticalityValue = #<your businessCriticality tag value here> The businessCriticality tag value you want to use. Example: "High"
 $tagPurposeName  = #<your purpose tag name here> The purpose tag name you want to use. Example:"Purpose"
 $tagPurposeValue = $purpose
+$tagSkuName = #<your SKU tag name here> The SKU tag name you want to use. Example:"Sku"
+$tagSkuValue = $storageAccountSkuName
 
 $global:currenttime= Set-PSBreakpoint -Variable currenttime -Mode Read -Action {$global:currenttime= Get-Date -UFormat "%A %m/%d/%Y %R"}
 $foregroundColor1 = "Red"
@@ -99,10 +104,9 @@ Set-Item Env:\SuppressAzurePowerShellBreakingChangeWarnings "true"
 
 ## Change the current context to use a management subscription
 
-$subNameTest = Get-AzSubscription | Where-Object {$_.Name -like "*management*"}
-$tenant = Get-AzTenant | Where-Object {$_.Name -like "*$companyShortName*"}
+$subNameManagement = Get-AzSubscription | Where-Object {$_.Name -like "*management*"}
 
-Set-AzContext -TenantId $tenant.TenantId -SubscriptionId $subNameTest.SubscriptionId | Out-Null 
+Set-AzContext -SubscriptionId $subNameManagement.SubscriptionId | Out-Null 
 
 Write-Host ($writeEmptyLine + "# Management subscription in current tenant selected" + $writeSeperatorSpaces + $currentTime)`
 -foregroundcolor $foregroundColor2 $writeEmptyLine
@@ -111,59 +115,59 @@ Write-Host ($writeEmptyLine + "# Management subscription in current tenant selec
 
 ## Store the specified set of tags in a hash table
 
-$tags = @{$tagSpokeName=$tagSpokeValue;$tagCostCenterName=$tagCostCenterValue;$tagCriticalityName=$tagCriticalityValue;$tagPurposeName=$tagPurposeValue}
+$tags = @{$tagSpokeName=$tagSpokeValue;$tagCostCenterName=$tagCostCenterValue;$tagCriticalityName=$tagCriticalityValue;$tagPurposeName=$tagPurposeValue;$tagSkuName=$tagSkuValue}
 
 Write-Host ($writeEmptyLine + "# Specified set of tags available to add" + $writeSeperatorSpaces + $currentTime)`
 -foregroundcolor $foregroundColor2 $writeEmptyLine 
 
 ## ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-## Create a dedicated resource group for Azure storage resources in the hub (also holds the Cloud Shell Azure Files share) if it not exists  
+## Create a resource group for the Cloud Shell resources if it not already exists. Also apply the necessary tags to this resource group
 
 try {
-    Get-AzResourceGroup -Name $rgStorageName -ErrorAction Stop | Out-Null 
+    Get-AzResourceGroup -Name $rgNameStorage -ErrorAction Stop | Out-Null 
 } catch {
-    New-AzResourceGroup -Name $rgStorageName -Location $region -Force | Out-Null 
+    New-AzResourceGroup -Name $rgNameStorage -Location $region -Force | Out-Null 
 }
 
 # Set tags rg storage
-Set-AzResourceGroup -Name $rgStorageName -Tag $tags | Out-Null
+Set-AzResourceGroup -Name $rgNameStorage -Tag $tags | Out-Null
 
-Write-Host ($writeEmptyLine + "# Resource group $rgStorageName available" + $writeSeperatorSpaces + $currentTime)`
+Write-Host ($writeEmptyLine + "# Resource group $rgNameStorage available" + $writeSeperatorSpaces + $currentTime)`
 -foregroundcolor $foregroundColor2 $writeEmptyLine
 
 ## ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-## Create a storage account for Cloud Shell if it not exists
+## Create a general purpose v2 storage account for Cloud Shell with specific configuration settings, if it not already exists. Also apply the necessary tags to this storage account.
 
 try {
-    Get-AzStorageAccount -ResourceGroupName $rgStorageName -Name $cloudShellStorageAccountName -ErrorAction Stop | Out-Null 
+    Get-AzStorageAccount -ResourceGroupName $rgNameStorage -Name $cloudShellStorageAccountName -ErrorAction Stop | Out-Null 
 } catch {
-    New-AzStorageAccount -ResourceGroupName $rgStorageName -Name $cloudShellStorageAccountName -SkuName $storageSKUNameStandardLRS -Location $region -Kind $storageAccountType `
+    New-AzStorageAccount -ResourceGroupName $rgNameStorage -Name $cloudShellStorageAccountName -SkuName $storageAccountSkuName -Location $region -Kind $storageAccountType `
     -AllowBlobPublicAccess $false -MinimumTlsVersion $storageMinimumTlsVersion | Out-Null 
 }
 
 # Set tags storage account
-Set-AzStorageAccount -ResourceGroupName $rgStorageName -Name $cloudShellStorageAccountName -Tag $tags | Out-Null
+Set-AzStorageAccount -ResourceGroupName $rgNameStorage -Name $cloudShellStorageAccountName -Tag $tags | Out-Null
 
 Write-Host ($writeEmptyLine + "# Storage account $cloudShellStorageAccountName created" + $writeSeperatorSpaces + $currentTime)`
 -foregroundcolor $foregroundColor2 $writeEmptyLine
 
 ## ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-## Create an Azure file share for Cloud Shell if it not exists
+## Create an Azure file share for Cloud Shell if it not exists. Also apply the necessary meta data to this file share
 
 $fileShareName = "$($purpose.ToString().ToLower())"
 
 try {
-    Get-AzRmStorageShare -ResourceGroupName $rgStorageName -StorageAccountName $cloudShellStorageAccountName -Name $fileShareName -ErrorAction Stop | Out-Null 
+    Get-AzRmStorageShare -ResourceGroupName $rgNameStorage -StorageAccountName $cloudShellStorageAccountName -Name $fileShareName -ErrorAction Stop | Out-Null 
 } catch {
-    New-AzRmStorageShare -ResourceGroupName $rgStorageName -StorageAccountName $cloudShellStorageAccountName -Name $fileShareName -AccessTier $fileShareAccessTier `
+    New-AzRmStorageShare -ResourceGroupName $rgNameStorage -StorageAccountName $cloudShellStorageAccountName -Name $fileShareName -AccessTier $fileShareAccessTier `
     -QuotaGiB $fileShareQuotaGiB | Out-Null 
 }
 
 # Set Metadata file share
-Update-AzRmStorageShare -ResourceGroupName $rgStorageName -StorageAccountName $cloudShellStorageAccountName -Name $fileShareName -Metadata $tags | Out-Null
+Update-AzRmStorageShare -ResourceGroupName $rgNameStorage -StorageAccountName $cloudShellStorageAccountName -Name $fileShareName -Metadata $tags | Out-Null
 
 Write-Host ($writeEmptyLine + "# Azure file share $fileShareName created" + $writeSeperatorSpaces + $currentTime)`
 -foregroundcolor $foregroundColor2 $writeEmptyLine
